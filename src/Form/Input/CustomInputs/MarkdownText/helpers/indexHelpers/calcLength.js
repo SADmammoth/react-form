9591f8ec36b81ae464e02e83dd477a25441b72d7
SADmammoth <lo.ma200018@gmail.com>
2020-09-15 16:30:52 +0300

    fix: console errors

--- src/Form/Input/CustomInputs/MarkdownText/helpers/indexHelpers/calcLength.js
+++ src/Form/Input/CustomInputs/MarkdownText/helpers/indexHelpers/calcLength.js
@@ -1,7 +1,10 @@
 export default function calcLength(text) {
   let regexp = /(?:<(?:[a-z0-9A-Z]+)[\s]*(?:[^<>]*)[\s]*>)|(?:<\/[^<>]+>)|(?:<(?:[a-z0-9A-Z]+)[\s]*(?:[^<>]*)[\s]*\/>)|(?:[\n])/g;
   let matches = [...text.matchAll(regexp)];
-  let tagsLength = matches.reduce((acc, match) => (acc += match[0].length), 0);
+  let tagsLength = matches.reduce(
+    (acc, match) => (acc += match[0] ? match[0].length : 0),
+    0
+  );
 
   return text.length - tagsLength + matches.length;
 }
