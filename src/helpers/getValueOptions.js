2774fbf5110eade58b98f3c2580439749806f7e0
SADmammoth <lo.ma200018@gmail.com>
2021-02-12 16:40:30 +0300

    fix: memory leak

--- src/helpers/getValueOptions.js
+++ src/helpers/getValueOptions.js
@@ -4,12 +4,22 @@ export default function useValueOptions(fetch) {
   let [valueOptions, setValueOptions] = useState(null);
 
   useEffect(() => {
+    let isUnmounted = false;
+
     if (fetch instanceof Function) {
-      fetch().then((options) => setValueOptions(options));
+      fetch().then((options) => {
+        if (!isUnmounted) {
+          setValueOptions(options);
+        }
+      });
     } else {
       setValueOptions(fetch);
     }
-  }, [fetch]);
+
+    return () => {
+      isUnmounted = true;
+    };
+  }, [fetch, setValueOptions]);
 
   return [valueOptions, !valueOptions];
 }
