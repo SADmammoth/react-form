dcff80626926d4448a7c809599713bda26ed1f13
SADmammoth <lo.ma200018@gmail.com>
2021-05-22 15:42:38 +0300

    feat: multistep form

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import Form from './Form';
+import MultiStepForm from './Form/MultiStepForm';
 import { MarkdownOutput } from './Input/CustomInputs/MarkdownText';
 import Validator from './Validator';
 import DateMaskConverters from './Validator/DateMaskConverters';
@@ -6,6 +7,7 @@ import DateMaskConverters from './Validator/DateMaskConverters';
 Form.MarkdownOutput = MarkdownOutput;
 Form.Validator = Validator;
 Form.DateMaskConverters = DateMaskConverters;
+Form.MultiStepForm = MultiStepForm;
 
 export default Form;
-export { MarkdownOutput, Validator, DateMaskConverters };
+export { MarkdownOutput, Validator, DateMaskConverters, MultiStepForm };
