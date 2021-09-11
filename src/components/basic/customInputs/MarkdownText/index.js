d96fd0ce50b67aeb940580297b17c3949de83ebc
SADmammoth <lo.ma200018@gmail.com>
2021-09-11 23:55:27 +0300

    fix exception

--- src/components/basic/customInputs/MarkdownText/index.js
+++ src/components/basic/customInputs/MarkdownText/index.js
@@ -1,6 +1,7 @@
 import MarkdownOutput from './MarkdownOutput';
 import MarkdownText from './MarkdownText';
-import './style.scss';
+
+// import './style.scss';
 
 export default MarkdownText;
 export { MarkdownOutput };
