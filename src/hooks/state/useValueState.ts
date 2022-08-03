4ead7159f221562afaa71d35b24a2c6897194b9e
Maxim <lo.ma200018@gmail.com>
2022-08-03 11:48:15 +0000

    UseValuesState

--- src/hooks/state/useValueState.ts
+++ src/hooks/state/useValueState.ts
@@ -1,9 +1,40 @@
+import { useCallback, useState } from 'react';
 import { InputsProps } from '../../types/InputsProps/InputsProps';
+import { SetValueCallback } from '../../types/SetValueCallback';
 import { UseValuesStateReturn } from '../../types/State/UseValueStateReturn';
 import { ValuesState } from '../../types/State/ValuesState';
+import { UpdateValueCallback } from '../../types/UpdateValueCallback';
 
 export function useValueState<InitInputsProps extends InputsProps>(
   initState: ValuesState<InitInputsProps>,
 ): UseValuesStateReturn<InitInputsProps> {
-  return {} as unknown as UseValuesStateReturn<InitInputsProps>;
+  const [values, setValues] = useState(initState);
+
+  const updateValue: UpdateValueCallback<InitInputsProps> = useCallback(
+    (name, value) => {
+      const oldValue = values[name];
+      if (oldValue.byCharValidator && oldValue.byCharValidator(value)) {
+        setValues({ ...values, [name]: { ...oldValue, value } });
+        return true;
+      }
+      //TODO Highlight Input
+      return false;
+    },
+    [values],
+  );
+
+  const setValue: SetValueCallback<InitInputsProps> = useCallback(
+    (name, value) => {
+      const oldValue = values[name];
+      if (oldValue.validator && oldValue.validator(value)) {
+        setValues({ ...values, [name]: { ...oldValue, value } });
+        return true;
+      }
+      //TODO Highlight Input
+      return false;
+    },
+    [values],
+  );
+
+  return [values, updateValue, setValue];
 }
